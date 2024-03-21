153fd67f978131f3886a715f84cc79b4cc5aa487
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-03-21 12:48:57 +0100

    In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Write a SQL script that creates a table users following these requirements:
+-- id, name, email, country
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
