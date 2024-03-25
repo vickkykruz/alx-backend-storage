172d9b30e6cb51e3df1b471950df4a92866a60ef
Victor Chuwkuemeka <onwuegbuchulemvic02@gmail.com>
2024-03-25 12:48:25 -0700

    Made changes

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,7 +6,5 @@ based on kwargs:
 
 def insert_school(mongo_collection, **kwargs):
     """ This function return the inserted document """
-    new_document = kwargs
-    result = mongo_collection.insert_one(new_document)
-    # Return the new _id
-    return result.inserted_id
+    result = mongo_collection.insert(kwargs)
+    return result
